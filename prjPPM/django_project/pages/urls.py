a575a155a9d786f074a67b962f1827cc6201c3bb
filograzzo <filippo.grazzini1@edu.unifi.it>
2024-06-12 11:26:46 +0200

    Fatta modalità per registrazione, log in, log out + creato degli html fittizzi per controllare che funzionassero

--- prjPPM/django_project/pages/urls.py
+++ prjPPM/django_project/pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import homePageView
+from . import views
 
 urlpatterns=[
-    path("", homePageView, name="home"),
+    path("", views.homePageView, name=""),
+    path("register", views.register, name="register"),
+    path("my-login", views.my_login, name="my-login"),
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("user-logout", views.user_logout, name="user-logout"),
 ]
